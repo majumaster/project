a7769d1c994bf0abd4d7ae5eff4510bc9e8744e9
291578umk <39299737+majumaster@users.noreply.github.com>
2018-06-10 10:10:30 +0200

    Add files via upload
    dodanie funkcji odpowiadającej za zaznaczanie klocków

--- projekt/java.js
+++ projekt/java.js
@@ -35,9 +35,9 @@ const allGra = {
 		//losowanie numerow dla obrazkow
 		 for (let i=this.liczbKlock-1; i>0; i--) {
             const los = Math.floor(Math.random()*i);
-            const obrazek = this.klocki[i];
-            this.klocki[i] = this.klocki[los];
-			this.klocki[los] = obrazek;
+            const obrazek = this.klockiLos[i];
+            this.klockiLos[i] = this.klockiLos[los];
+			this.klockiLos[los] = obrazek;
         }
 		
 		//układanie obrazków na planszy
@@ -45,11 +45,32 @@ const allGra = {
 			const obraz = document.createElement('div');
 			obraz.classList.add('styleKart');
 			this.divPlansza.appendChild(obraz);
+			obraz.dataset.cardType = this.klockiLos[i];
+            obraz.dataset.index = i;
 			
 			obraz.addEventListener('click', this.obrazClick.bind(this));
 		}
+		canGet : true, //czy można klikać na klocki
+		klockiPary : 0, //liczba znalezionych par
 		obrazClick : function(e) {
-			if(!klockZazn[0] || )
+			if (!this.klockiZazn[0] || (this.klockiZazn[0].dataset.index !== e.target.dataset.index)) {
+                this.klockiZazn.push(e.target);
+                e.target.style.backgroundImage = 'url(' + this.klockiImg[e.target.dataset.cardType] + ')';
+            }
+
+            if (this.klockiZazn.length === 2) {
+                this.canGet = false; //zablokowanie mozliwosci wybierania klockow po zaznaczeniu 2 klockow
+
+                if (this.klockiZazn[0].dataset.cardType === this.klockiZazn[1].dataset.cardType) {
+                    setTimeout(this.usunKlocek.bind(this), 500);
+                } else {
+                    setTimeout(this.schowajKlocek.bind(this), 500);
+                }
+
+                this.iloscRuch++;
+                this.divWynik.innerHTML = this.iloscRuch;
+            }
+        
 		}
 		document.querySelector('.start').addEventListener('click', function() {
          allGra.(rozpoczGre);});
